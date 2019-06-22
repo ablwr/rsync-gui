d35e5a5099f2cfb2c68d8049a0b2e55c384014f3
Ashley Blewer <ashley.blewer@gmail.com>
2019-06-22 16:52:57 -0400

    Add updating script viewer feature

--- js/js.js
+++ js/js.js
@@ -1,5 +1,31 @@
 const { dialog } = require('electron').remote
 
+// scriptBuilder
+
+let scriptParts = {
+  rsync: "rsync",
+  flags: "", // if empty add -, or add at end
+  sourceServer: "",
+  source: "",
+  destServer: "",
+  dest: "",
+}
+
+function scriptBuilder(scriptParts) {
+  builtScript = `${scriptParts.rsync} ${scriptParts.source} ${scriptParts.dest}` 
+  console.log(builtScript);
+  document.getElementById('showScript').innerHTML = builtScript
+}
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach(function (b, i){
+  b.addEventListener("click", (e) => {
+    scriptBuilder(scriptParts, "x")
+  })
+})
+
+// Selecting places
+
 const selectSourceBtn = document.getElementById('source-directory')
 const selectDestBtn = document.getElementById('destination-directory')
 
@@ -8,6 +34,8 @@ selectSourceBtn.addEventListener("click", (event) => {
     properties: ['openDirectory']
   });
   document.getElementById('selected-source').innerHTML = `Source: ${selectedSource}`
+  scriptParts.source = selectedSource
+  scriptBuilder(scriptParts)
 })
 
 selectDestBtn.addEventListener("click", (event) => {
@@ -15,4 +43,6 @@ selectDestBtn.addEventListener("click", (event) => {
     properties: ['openDirectory']
   })
   document.getElementById('selected-dest').innerHTML = `Destination: ${selectedDest}`
+  scriptParts.dest = selectedDest
+  scriptBuilder(scriptParts)
 })
