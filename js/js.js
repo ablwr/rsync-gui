43220b3aed9d356ba33f05e13cf50036027e8768
Ashley Blewer <ashley.blewer@gmail.com>
2019-06-21 23:18:59 -0400

    Add some CSS

--- js/js.js
+++ js/js.js
@@ -3,7 +3,6 @@ const { dialog } = require('electron').remote
 const selectSourceBtn = document.getElementById('source-directory')
 const selectDestBtn = document.getElementById('destination-directory')
 
-console.log("hi")
 selectSourceBtn.addEventListener("click", (event) => {
   selectedSource = dialog.showOpenDialog({
     properties: ['openDirectory']
@@ -15,5 +14,5 @@ selectDestBtn.addEventListener("click", (event) => {
   selectedDest = dialog.showOpenDialog({
     properties: ['openDirectory']
   })
-  document.getElementById('selected-dest').innerHTML = `Source: ${selectedDest}`
+  document.getElementById('selected-dest').innerHTML = `Destination: ${selectedDest}`
 })
